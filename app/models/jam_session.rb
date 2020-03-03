3f0df0ae79177fbc836a15bc91559d2d85bcc47f
Olivier Girardot <olive_girardot@hotmail.com>
2020-03-03 11:47:54 +0100

    validations and has_many

--- app/models/jam_session.rb
+++ app/models/jam_session.rb
@@ -1,3 +1,10 @@
 class JamSession < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  validates :title, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 45 }
+  validates :genre, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :location
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,5 @@
 class Spot < ApplicationRecord
   belongs_to :jam_session
   belongs_to :instrument
+  has_many :participations
 end
