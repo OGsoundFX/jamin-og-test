3316dedb57224317e691549dc3746d8c7c45ca2c
Olivier Girardot <olive_girardot@hotmail.com>
2020-03-03 13:34:25 +0100

    validations and has_many3

--- app/models/jam_session.rb
+++ app/models/jam_session.rb
@@ -1,9 +1,8 @@
 class JamSession < ApplicationRecord
   belongs_to :user
-  # validates :title, presence: true, length: { minimum: 3, maximum: 15 }
-  # validates :description, presence: true, length: { minimum: 10, maximum: 45 }
-  # validates :genre, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 45 }
+  validates :genre, presence: true, length: { minimum: 3, maximum: 15 }
   validates :starts_at, presence: true
   validates :ends_at, presence: true
-  validates :location
 end
