d9df10c2cec4d88c954dcf1fc6329d8101de8e0c
florian gaebler <floriangaebler87@gmail.com>
2020-03-04 18:16:57 +0100

    searchbar and bannerpage

--- app/controllers/jam_sessions_controller.rb
+++ app/controllers/jam_sessions_controller.rb
@@ -1,2 +1,54 @@
 class JamSessionsController < ApplicationController
+  def index
+    if params[:location].present?
+      # @jam_sessions = JamSession.search_by_location(params[:query])
+      @jam_sessions = JamSession.joins(spots: :instrument).where(instruments: { id: params[:instrument_id] }).search_by_location(params[:location])
+    else
+      @jam_sessions = JamSession.all
+    end
+    # geocode(@jam_sessions)
+  end
+
+  # def new
+  # @jam_session = JamSession.new
+  # end
+
+  #   def show
+  # @user = current_user
+  # @jam_session = JamSession.find(params[:id])
+  # @participations = Participation.all.select { |participation| participation. == @jam_session }
+  # @booking = Booking.new
+  # @users = User.all
+  # @booking.property = @property
+  #   end
+
+  #   # create new properties
+
+  #   def create
+  # @jam_session = JamSession.new(jam_session_params)
+  # @jam_session.user = current_user
+  # if @jam_session.save
+  #   redirect_to jam_session_path(@jam_session)
+  # else
+  #   render "new"
+  # end
+  #   end
+
+  private
+
+  # def geocode(jam_sessions)
+  # jam_sessions.geocoded #returns flats with coordinates
+  # @markers = jam_sessions.map do |jam_session|
+  # {
+  # lat: jam_session.latitude,
+  # lng: jam_session.longitude,
+  # infoWindow: render_to_string(partial: "info_window", locals: { jam_session: jam_session }),
+  # image_url: helpers.asset_url("custom_marker.png"),
+  # }
+  # end
+  # end
+
+  def jam_session_params
+    params.require(:jam_session).permit(:title, :description, :genre, :starts_at, :ends_at, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:show, :edit, :update]
+  resources :jam_sessions, only: [:index, :create, :show, :new]
 
   namespace :user do
     root :to => "profiles#edit"
