55c398892f679424106e688adde184dd465fd82b
Leoni Sperling <leoni.sperling@gmail.com>
2020-03-05 23:51:06 +0100

    add image of jam session creator

--- app/controllers/jam_sessions_controller.rb
+++ app/controllers/jam_sessions_controller.rb
@@ -21,6 +21,11 @@ class JamSessionsController < ApplicationController
 
   def show
     @jam_session = JamSession.find(params[:id])
+    @markers =   [{
+        lat: @jam_session.latitude,
+        lng: @jam_session.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { jam_session: @jam_session })
+      }]
   end
 
 
