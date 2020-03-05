fd35b266f4ec060f1e218650541fca3c202d766b
Leoni Sperling <leoni.sperling@gmail.com>
2020-03-05 14:14:35 +0100

    add new links for user images

--- db/seed_users.rb
+++ db/seed_users.rb
@@ -16,7 +16,7 @@ users = [
     first_name: "Bob",
     last_name: "Sponge",
     bio: "I live in the sea",
-    photo_url: 'https://images.unsplash.com/photo-1522536421511-14c9073df899?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_1_w88pku.jpg',
   },
   {
     email: "oliv@gmail.com",
@@ -24,7 +24,7 @@ users = [
     first_name: "Olivier",
     last_name: "Girardot",
     bio: "I believe I can fly",
-    photo_url: 'https://cdn-soundlister.pressidium.com/wp-content/uploads/formidable/6/Photo-2017-300x300.jpeg',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_2_dip4wm.jpg',
   },
   {
     email: "miguel@lopez.com",
@@ -32,7 +32,7 @@ users = [
     first_name: "Miguel",
     last_name: "Lopez",
     bio: "Let's get together and experiment. I love to broaden my horizons",
-    photo_url: 'https://images.unsplash.com/photo-1529518969858-8baa65152fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_3_lkwf5j.jpg',
   },
   {
     email: "maria@mueller.com",
@@ -40,7 +40,7 @@ users = [
     first_name: "Maria",
     last_name: "Mueller",
     bio: "I really enjoy classical and rock music! Contradicting I know, but life is full of surprices!",
-    photo_url: 'https://images.unsplash.com/photo-1563379055946-c5dbcd7bd610?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_4_micpcb.jpg',
   },
   {
     email: "lara@lara.com",
@@ -48,7 +48,7 @@ users = [
     first_name: "Lara",
     last_name: "Croft",
     bio: "Recently moved here and looking to expand my network by jamming with other musicians. Singing is my passion!",
-    photo_url: 'https://images.unsplash.com/photo-1455503521443-c39d5b861bc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_5_gvebti.jpg',
   },
   {
     email: "amy@weinhaus.com",
@@ -56,7 +56,7 @@ users = [
     first_name: "Amy",
     last_name: "Weinhaus",
     bio: "I love to sing, not only in the shower! I also like to play the guitar!",
-    photo_url: 'https://images.unsplash.com/photo-1558499737-ff02d688f9da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    photo_url: 'https://res.cloudinary.com/dqbzzl00v/image/upload/v1583413973/user-imgs/user_6_oehmid.jpg',
   },
 ]
 
