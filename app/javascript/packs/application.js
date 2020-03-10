3f7724a3093bf6a4506030e644d008f324a5fde3
Leoni Sperling <leoni.sperling@gmail.com>
2020-03-10 10:39:55 +0100

    try to fix flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,5 @@ flatpickr(".TimePicker", {
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
- 
+
 initMapbox();
