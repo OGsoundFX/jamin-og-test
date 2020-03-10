87348bc852b72fc6ad96699aad9af8693d6ab9cd
florian gaebler <floriangaebler87@gmail.com>
2020-03-10 18:35:18 +0100

    participation fx and session creator

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jam_sessions, only: [:show, :index, :new, :create] do
-    resources :spots, only: [:show]
+    resources :spots, only: [:show, :create, :new]
     resources :messages, only: [:create]
   end
   resources :participations, only: [:create, :update]
 
   resources :profiles, only: [:show, :edit, :update]
   resources :dashboards, only: [:index, :show]
-
 
   # namespace :user do
   #   root :to => "profiles#edit"
