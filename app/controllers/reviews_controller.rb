c4b0bb93249e48c65c27b9e51b4a0f2d9780bc4d
Olivier Girardot <olive_girardot@hotmail.com>
2020-03-11 12:43:11 +0100

    adding reviews to profile

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,11 @@ class ReviewsController < ApplicationController
   # Find who the writer is (person who's logged in) and set the review's writer
   # Find who the review is for (from the url/params) and set the review's receiver
   # Save the review and if successful redirect to where you want
+  def index
+    @receiver = User.find(params["user_id"])
+    @reviews = Review.where(receiver_id: @receiver.id)
+  end
+
   def create
     @review = Review.new(reviews_params)
     @writer = current_user
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :writer, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validates :rating, inclusion: { in: (1..5) }
+  validates :content, length: { minimum: 5 }
 end
