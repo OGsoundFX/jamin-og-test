1210b33bf349dab3c61d46eeed038948160a0cab
florian gaebler <floriangaebler87@gmail.com>
2020-03-11 12:10:44 +0100

    changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import { initMapbox } from '../plugins/init_mapbox';
 
 flatpickr(".TimePicker", {
     // enableTime: true,
