3623aa535709dfd9247213339f2df6407f71ab43
Olivier Girardot <olive_girardot@hotmail.com>
2022-09-22 11:48:37 +0200

    included render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,1 +1,10 @@
-bundle exec puma -C config/puma.rb
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
