120ad1f8b751c53b6d7519f94ad4ddd9b5e48c22
Olivier Girardot <olive_girardot@hotmail.com>
2022-09-27 11:22:15 +0200

    added plan:free to the render.yaml file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,7 @@ bundle install
 yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails db:drop
+bundle exec rails db:create
+bundle exec rails db:migrate
 bundle exec rails db:seed
