fc8c5b5c01c3c42eb6e8030aaefff69d0a157b5c
Olivier Girardot <olive_girardot@hotmail.com>
2022-10-03 15:25:30 +0200

    removed bundle commands in render_build.sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# bundle install
-# yarn install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
-# bundle exec rails db:migrate
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rails db:migrate
 # bundle exec rails db:seed
